062f4b4526ca6af51ba08ea95bccd12636317834
dishxpe <joaquin.montilla@outlook.com>
2025-09-09 19:40:56 +0800

    Initial commit: "File Manipulation API"

--- src/main/java/com/collection/univapi/UnivapiApplication.java
+++ src/main/java/com/collection/univapi/UnivapiApplication.java
@@ -2,12 +2,12 @@ package com.collection.univapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class UnivapiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UnivapiApplication.class, args);
     }
-    
 }
