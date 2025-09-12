c4dfc7790773eea0bd111205052d649a22064579
dishxpe <joaquin.montilla@outlook.com>
2025-09-13 01:10:23 +0800

    initial commit initializers

--- src/main/java/com/collection/univapi/api/model/FileMetadata.java
+++ src/main/java/com/collection/univapi/api/model/FileMetadata.java
@@ -1,49 +1,15 @@
 package com.collection.univapi.api.model;
 
+import lombok.*;
+import java.time.Instant;
+
+@Getter
+@AllArgsConstructor
 public class FileMetadata implements FileInfo {
     private String fileName;
     private String directory;
     private long size;
     private String mimeType;
-    private java.time.Instant lastModified;
+    private Instant lastModified;
     private String hash;
-
-    public FileMetadata(String fileName, String directory, long size, String mimeType, java.time.Instant lastModified, String hash) {
-        this.fileName = fileName;
-        this.directory = directory;
-        this.size = size;
-        this.mimeType = mimeType;
-        this.lastModified = lastModified;
-        this.hash = hash;
-    }
-
-    @Override
-    public String getFileName() {
-        return fileName;
-    }
-
-    @Override
-    public String getDirectory() {
-        return directory;
-    }
-
-    @Override
-    public long getSize() {
-        return size;
-    }
-
-    @Override
-    public String getMimeType() {
-        return mimeType;
-    }
-
-    @Override
-    public java.time.Instant getLastModified() {
-        return lastModified;
-    }
-
-    @Override
-    public String getHash() {
-        return hash;
-    }
 }
